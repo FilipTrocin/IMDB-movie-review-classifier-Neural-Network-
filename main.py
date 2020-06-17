35d4b16ade953a8278ae81b69d33870ae79e9939
b9033970 <b9033970@newcastle.ac.uk>
2020-06-17 08:22:51 +0200

    added methods for preprocessing test,train data - Defined padding. Created function which is decoding numerical values into text

--- main.py
+++ main.py
@@ -15,10 +15,29 @@ word_index['<PAD>'] = 0
 word_index['<START>'] = 1
 word_index['<UNK>'] = 2
 word_index['<UNUSED>'] = 3
+
 # Reversing key and values together in dictionary
 reversed_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
+# Preprocessing data: defined padding (making it the same length)
+train_dt = keras.preprocessing.sequence.pad_sequences(train_dt, maxlen=2494, value=word_index['<PAD>'], padding='post')
+test_dt = keras.preprocessing.sequence.pad_sequences(test_dt, maxlen=2494, value=word_index['<PAD>'], padding='post')
 
 
 
+def decoder(text_item, index_of_words):
+    # Swapping number with a corresponding word to it
+    len_arr = []
+    for item in range(len(text_item)):
+        string = ''
+        str_length = 0
+        for word in text_item[item]:
+            string = string + index_of_words[word] + ' '
+            str_length += 1
+        # print(string)
+        len_arr.append(str_length)
+    # print('Number of most words in review: ', max(len_arr))
+
+
+decoder(train_dt, reversed_word_index)
 
